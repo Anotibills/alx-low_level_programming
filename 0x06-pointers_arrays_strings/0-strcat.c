3762b00465818b36128f1055cbf6422bbbf512b7
Anotibills <anotibills@gmail.com>
2023-03-30 12:44:45 -0700

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,28 +2,27 @@
 
 /**
  * _strcat - concatenates two strings
+ *
  * @dest: destination
  * @src: source
+ *
  * Return: dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
-	i = 0;
+        int destlen = 0;
+        int srclen = 0;
+        int i;
 
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
+        for (i = 0 ; dest[i] != '\0' ; i++)
+                destlen++;
+        for (i = 0 ; src[i] != '\0' ; i++)
+                srclen++;
+        for (i = 0 ; i <= srclen ; i++)
+                dest[destlen + i] = scr[i];
+        return (dest);
+}
+';
 	return (dest);
 }
