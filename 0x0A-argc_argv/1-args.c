9b6e1edb4b9065b573a852d4f49525dfa3ee1042
Tolulope Abiola <oyinbills01@gmail.com>
2023-04-10 07:11:58 -0700

    silence is argument task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - to print number of arguments passed
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Number of arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+		printf("%d\n", argc);
+		return (0);
+}
