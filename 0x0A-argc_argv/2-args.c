b5bc4033f9273a7af3909958e530a0993902aa65
Tolulope Abiola <oyinbills01@gmail.com>
2023-04-10 07:26:01 -0700

    Program that prints arguments received

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints all arguments received
+ * @argc: numbers of arguments
+ * @argv: array of arguments
+ * Return: argument received
+ */
+int main(int argc, *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
