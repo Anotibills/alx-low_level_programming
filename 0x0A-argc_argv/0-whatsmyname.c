76e2b9c18ff60af3ad77cb8fce04956b201f1148
Tolulope Abiola <oyinbills01@gmail.com>
2023-04-10 05:46:28 -0700

    whats my name task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,10 +4,11 @@
  * main - to print program name
  * @argc: number of argument
  * @argv: arrays of argument
+ *
  * Return: Program name
  */
 
-int main(int argc, char *argv[])
+int main(int argc_attribute_((unused)), char *argv[])
 {
 	printf("%s\n", *argvi);
 	return (0);
