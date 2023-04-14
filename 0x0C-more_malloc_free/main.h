57afa63fd53135ec553636899efc6697fb907acb
Anotibills <anotibills@gmail.com>
2023-04-13 21:56:04 -0700

    More malloc and free task corrected

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
+#include <string.h>
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
