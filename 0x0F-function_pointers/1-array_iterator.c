0fe978c7e08e242c3a9e4dbb89512158a1fe176e
Tolulope Abiola <oyinbills01@gmail.com>
2023-04-19 15:44:45 -0700

    int_index task solved

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,12 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator
+ * array_iterator - executes a function given as a parameter in array
+ * @array: the array
+ * @size: size of array
+ * @action: pointer to the function
+ * Return: Always o
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	if (array == NULL || action == NULL)
