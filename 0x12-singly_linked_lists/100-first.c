b5f5d6237e2f32d8dae46ce8f6a3f80aaf897701
anoti <anotibills@gmail.com>
2023-04-27 22:17:29 -0700

    100 corrected

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include "lists.h"
 
 /**
  * first - function that prints a sentence
  *
  * done
  */
-void first(void) __attribute__ ((constructor))
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
