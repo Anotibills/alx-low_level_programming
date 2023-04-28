1385dbc50aa02a223ede43bebbaf2ee8d85bfc91
anoti <anotibills@gmail.com>
2023-04-27 22:05:25 -0700

    corrected

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,8 +11,8 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *n;
 	list_t *temp = *head;
-	unsigned int len = 0;
-	
+	unsigned int len = 0;	
+
 	while (str[len])
 	len++;
 	n = malloc(sizeof(list_t));
