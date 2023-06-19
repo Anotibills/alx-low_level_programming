20dda39e29cf9919a32d74e55ce743ef767abccd
anoti <anotibills@gmail.com>
2023-06-19 05:33:03 -0700

    Files to push

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strchr - locates a character in a string
+ * _strchr - function that locates C in a string
  * @s: pointer to character
  * @c: character in string
  * Return: If c is found - a pointer to the first occurence
@@ -9,12 +9,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-	/**i is index*/
-	for (i = 0; s[i] >= '\0'; i++)
-	{
-		if (s[i] == c)
-			return (s + i);
-	}
-	return ('\0');
+        int i;
+        /**i is index*/
+        for (i = 0; s[i] >= '\0'; i++)
+        {
+                if (s[i] == c)
+                        return (s + i);
+        }
+        return ('\0');
 }
