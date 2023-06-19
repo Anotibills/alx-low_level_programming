aa5b4af4c32f9a30952726206874e90159f8e209
anoti <anotibills@gmail.com>
2023-06-19 04:58:37 -0700

    solviny Dynamic libraries task

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates C in a string
+ * @s: pointer to the character
+ * @c: character in string
+ * Return: If c is found - a pointer to the first occurence
+ * If c is not found - NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	/**i is index*/
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return ('\0');
+}
---
+++ 0x18-dynamic_libraries/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare string
+ * @s1: string
+ * @s2: string
+ * Return: value less than 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0')
+		i++;
+	j = s1[i] - s2[i];
+	return (j);
+}
