e49c7fcc5b2a12c05932f1338a6c6f5962da562a
anoti <anotibills@gmail.com>
2023-06-21 23:45:48 -0700

    Function that calls C from python task

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - function that does addition
+ * @a: integer to add
+ * @b: integer to add
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - function that subtrates numbers
+ * @a: first number
+ * @b: second number
+ * Return: solution
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - function that multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: Multiplication solution
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Function that divides
+ * @a: first number
+ * @b: second number
+ * Return: Solution
+ */
+int div(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * mod - function that gives module
+ * @a: first number
+ * @b: second number
+ * Return: module
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
